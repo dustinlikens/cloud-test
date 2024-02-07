e7e187a4deb8c11b61e038430d8378c423782435
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-02-06 21:20:34 -0800

    xcodebuild

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,5 +7,18 @@
 #  
 #find / -name '*.log'
 #grep -r "Build settings from command line" /Volumes
-cat /Volumes/workspace/ci/*/o.out
+#cat /Volumes/workspace/ci/*/o.out
+echo $CI_PROJECT_FILE_PATH
+mkdir /Volumes/workspace/veracode
+cd /Volumes/workspace/veracode
+xcodebuild archive \
+  -project $CI_PROJECT_FILE_PATH \
+  -scheme cloud-test \
+  -destination generic/platform=iOS \
+  -configuration Debug \
+  -archivePath Veracode.xcarchive \
+  DEBUG_INFORMATION_FORMAT=dwarf-with-dsym \
+  ENABLE_BITCODE=NO \
+  > build.log.txt
+cat build.log.txt
 exit 0
