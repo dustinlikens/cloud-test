a89e7150e86fc59d6336e586fab7aeba92250a78
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-02-07 16:15:51 -0800

    mv

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -13,12 +13,12 @@
 #echo $CI_BUILD_ID
 #echo $CI_BUILD_URL
 cd /Volumes/workspace/ci
-#cd "$(\ls -1dt ./*/ | head -n 1)"
-grep -r "ARCHIVE SUCCEEDED" .
+cd "$(\ls -1dt ./*/ | head -n 1)"
+#grep -r "ARCHIVE SUCCEEDED" .
 #mkdir /Users/local/.veracode
 #ls .
-#cp o.out o.txt
-#cat o.txt
+mv o.out o.txt
+cat o.txt
 #cat /Users/local/.veracode/log.txt
 #ls .
 #cd "$( find "$1"/* -prune -type d 2>/dev/null | xargs stat -c "%Y %n" 2>/dev/null | sort -nr | head -n 1 | cut -d " " -f 2-)"
