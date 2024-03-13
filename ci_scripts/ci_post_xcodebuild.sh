c7f9faf99a4686821a8cdfbebba0298a575cdb1a
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-03-13 14:47:58 -0700

    CI_WORKSPACE_PATH

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,9 +11,12 @@
 #cat /Volumes/workspace/ci/*/o.out
 #ls /Volumes/workspace/ci
 
-pwd
+echo $CI_WORKSPACE_PATH
+ls -R $CI_WORKSPACE_PATH
+
+#pwd
 #find / -name "xcodebuild-archive*" 2>&1 | grep -v "Permission denied"
-ls -R /Volumes/workspace/ci
+#ls -R /Volumes/workspace/ci
 #mkdir /Volumes/workspace/veracode
 #/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild archive -project /Volumes/workspace/repository/cloud-test.xcodeproj -scheme cloud-test -destination generic/platform=iOS -archivePath /Volumes/workspace/build-v.xcarchive -derivedDataPath /Volumes/workspace/DerivedData -IDEPostProgressNotifications=YES CODE_SIGN_IDENTITY=- AD_HOC_CODE_SIGNING_ALLOWED=YES CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM=$CI_TEAM_ID COMPILER_INDEX_STORE_ENABLE=NO -hideShellScriptEnvironment DEBUG_INFORMATION_FORMAT=dwarf-with-dsym ENABLE_BITCODE=NO > /Volumes/workspace/veracode/build-log.txt
 #cat /Volumes/workspace/veracode/build-log.txt
