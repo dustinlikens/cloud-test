29592db3bd658f5d03ee2fffbf90642ae4a3bf0c
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-03-13 15:08:53 -0700

    ls derived data

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,6 +11,11 @@
 #cat /Volumes/workspace/ci/*/o.out
 #ls /Volumes/workspace/ci
 
+
+
+echo $CI_DERIVED_DATA_PATH
+ls -R $CI_DERIVED_DATA_PATH
+
 #echo $CI_WORKSPACE_PATH
 #ls -R $CI_WORKSPACE_PATH
 
@@ -26,9 +31,9 @@
 #cp -R $CI_DERIVED_DATA_PATH/Logs/Build/*.xcactivitylog
 #echo $CI_BUILD_ID
 #echo $CI_BUILD_URL
-cd /Volumes/workspace/ci
-cd "$(\ls -1dt ./*/ | head -n 1)"
-cat o.out
+#cd /Volumes/workspace/ci
+#cd "$(\ls -1dt ./*/ | head -n 1)"
+#cat o.out
 #chmod -x o.out
 #mkdir /Users/local/custom
 #sudo sed 's/^.\{31\}//' o.out >> /Users/local/custom/build-log.txt
